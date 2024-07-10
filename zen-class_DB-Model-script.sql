227db396cc599501dfad388e68384da04814fbfe
issei416 <mukeshkj2912@gmail.com>
2024-07-10 21:44:39 +0530

    updated script

--- zen-class_DB-Model-script.sql
+++ zen-class_DB-Model-script.sql
@@ -45,7 +45,87 @@ DESCRIBE batches;
 
 CREATE TABLE marks (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    student_id INt,check
-)
+    student_id INT,
+    task_id INT,
+    mark FLOAT,
+    eval_mentor_id INT,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (eval_mentor_id) REFERENCES mentors(id)
+);
+
+DESCRIBE marks;
+
+CREATE TABLE students (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name TEXT,
+    course_id INT,
+    batch_id INT,
+    tasks_completed INT,
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    FOREIGN KEY (batch_id) REFERENCES batches(id)
+);
+
+ALTER TABLE marks ADD FOREIGN KEY (student_id) REFERENCES students(id);
+
+DESCRIBE students;
+
+DESCRIBE marks;
+
+CREATE TABLE  dashboard (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    tasks_completed INT,
+    marks_totals FLOAT,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+DESCRIBE dashboard;
+
+CREATE TABLE leaderboard (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    student_rank INT,
+    FOREIGN KEY (student_id) REFERENCES students(id)
+);
+
+DESCRIBE leaderboard;
+
+CREATE TABLE queries (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    assigned_mentor_id INT,
+    query_tag TEXT,
+    query_status TINYINT,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (assigned_mentor_id) REFERENCES mentors(id)
+);
+
+DESCRIBE queries;
+
+CREATE TABLE feedbacks (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    batch_id INT,
+    student_id INT,
+    mentor_id INT,
+    session_rating FLOAT,
+    session_feedback TEXT,
+    mentor_rating FLOAT,
+    mentor_feedback TEXT,
+    FOREIGN KEY (batch_id) REFERENCES batches(id),
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (mentor_id) REFERENCES mentors(id)
+);
+
+DESCRIBE feedbacks;
+
+SHOW TABLES from zen_class;
+
+
+
+
+
+
+
+
 
 
